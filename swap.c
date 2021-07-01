9b6dce3a443d3125f2fb4f19b4fe1a36913dc03d
blaze7b <84660073+blaze7b@users.noreply.github.com>
2021-07-01 19:52:40 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -14,14 +14,18 @@ void swap_max(int arr[],int l, int n) {
 	}
 	
 void ssort(int arr[],int l) {
-	int largest=0,index,temp;
-	for(int i=0;i<l;i++) {
-		if(arr[i]>largest) {
-			largest=arr[i];
-			index=i;
-			temp=arr[i];
-			arr[i]=arr[index];
-			arr[index]=temp;
+	int largest=arr[0],b=0,index,temp;
+		for(int j=0;j<l;j++) {
+			for(int i=b;i<l;i++) {
+				if(arr[i]>=largest) {
+					largest=arr[i];
+					index=i;
+					}
 				}
+			temp=arr[b];
+			arr[b]=arr[index];
+			arr[index]=temp;
+			b++;
+			largest=arr[b];
 			}
 		}
